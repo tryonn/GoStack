f12073bfbcc5ee0e28d4c4e851d4409bad0b2180
Simão Menezes <simaomenezesneto@gmail.com>
2020-04-13 17:16:25 +0100

    Implementando primeira entidade e sua rota, com algumas validação de datas.

---
+++ nivel_02/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -0,0 +1,39 @@
+import { Router, request, response } from 'express';
+import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+
+const appointmentsRouter = Router();
+
+interface Appointment {
+  id: string,
+  provider: string,
+  date: Date
+}
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parseDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find( appointment => isEqual(parseDate, appointment.date),);
+
+  if (findAppointmentInSameDate){
+    return response.status(400).json({ message: 'This appointment is already booked' });
+  }
+
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parseDate,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointments);
+});
+
+export default appointmentsRouter;
--- nivel_02/primeiro-projeto-node/src/server.ts
+++ nivel_02/primeiro-projeto-node/src/server.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
 
 import Route from './routes';
 
 const app = express();
+
+app.use(express.json());
 app.use(Route);
 
 app.listen(3333, () => {
