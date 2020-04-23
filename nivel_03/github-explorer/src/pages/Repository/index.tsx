d63a4ae2cfb893eeaf7d60dd3a463310dc99ab45
Simão Menezes <simaomenezesneto@gmail.com>
2020-04-24 00:32:03 +0100

    Criando css e layout da tela de detalhes dos repositories.

--- nivel_03/github-explorer/src/pages/Repository/index.tsx
+++ nivel_03/github-explorer/src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoImg from '../../assets/github-logo.svg';
 
 
 interface RepositoryParams {
@@ -8,9 +13,52 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  console.log(params.repository);
+  return(
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer"/>
+        <Link to="/dashboard">
+          <FiChevronLeft size={16}/>
+            voltar
+        </Link>
+      </Header>
 
-  return <h1>Repository:{params.repository}</h1>
-}
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars0.githubusercontent.com/u/1?v=4" alt="guthub-repository"/>
+          <div>
+            <strong>dadasjdas/adsa</strong>
+            <p>anfasdna</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1212</strong>
+            <span>stars</span>
+          </li>
+          <li>
+            <strong>78</strong>
+            <span>Issues</span>
+          </li>
+
+          <li>
+            <strong>78</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+          <Link to="wdeqwe">
+            <div>
+              <strong>eqwewq</strong>
+              <p>rqweqwe</p>
+            </div>
+              <FiChevronRight size={20} />
+          </Link>
+      </Issues>
+    </>
+  );
+};
 
 export default Repository;
