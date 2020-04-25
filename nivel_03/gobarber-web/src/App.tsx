75721f1338aa90a6f648fdd86da47b148e84ec08
Simão Menezes <simaomenezesneto@gmail.com>
2020-04-25 14:51:56 +0100

    Configurando estrutura do GoBarber

---
+++ nivel_03/gobarber-web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => (
+
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyles />
+
+  </>
+);
+
+
+export default App;
