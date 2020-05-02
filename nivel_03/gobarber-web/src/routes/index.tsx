f0bb848e6797d5a0cfb96306fdd6631f624360f3
Simão Menezes <simaomenezesneto@gmail.com>
2020-05-02 18:49:51 +0100

    Finalizando Rotas autenticadas

--- nivel_03/gobarber-web/src/routes/index.tsx
+++ nivel_03/gobarber-web/src/routes/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../Dashboard';
 
 const Routes: React.FC = () => (
     <Switch>
         <Route path="/" exact component={SignIn} />
         <Route path="/signup" exact component={SignUp} />
+        <Route path="/dashboard" exact component={Dashboard} isPrivate/>
 
     </Switch>
 );
