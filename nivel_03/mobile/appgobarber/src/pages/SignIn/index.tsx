e41d67ecd8c550eb707029be53f7a329f56b1912
Simão Menezes <simaomenezesneto@gmail.com>
2020-05-03 18:24:37 +0100

    Configurando rotas da aplicação

---
+++ nivel_03/mobile/appgobarber/src/pages/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+
+const SingIn: React.FC = () => (
+
+    <View />
+
+);
+
+
+export default SingIn;
---
+++ nivel_03/mobile/appgobarber/src/pages/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+
+const SignUp: React.FC = () => (
+
+    <View />
+
+);
+
+
+export default SignUp;
