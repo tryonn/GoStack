dd84fad76f74b817c87e27504dade366c962acbe
Simão Menezes <simaomenezesneto@gmail.com>
2020-05-05 23:43:33 +0100

    Foco e preenchimento, adicionando styles aos input

--- nivel_03/mobile/appgobarber/src/components/input/index.tsx
+++ nivel_03/mobile/appgobarber/src/components/input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
+import React, { useEffect, useRef, useImperativeHandle, forwardRef, useState, useCallback } from 'react';
 
 import { TextInputProps } from 'react-native';
 
@@ -32,6 +32,22 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = ({ name, icon,
     const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
 
+
+    const [ isFocused, setIsFocused ] = useState(false);
+    const [ isFilled, setIsFilled ] = useState(false);
+
+    const handleInputFocus = useCallback(() => {
+        setIsFocused(true);
+    }, []);
+
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false);
+        setIsFilled(!!inputValueRef.current.value);
+    }, []);
+
+
+
+
     useImperativeHandle(ref, () => ({
         focus(){
             inputElementRef.current.focus();
@@ -60,12 +76,14 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = ({ name, icon,
 
 
     return (
-        <Container>
-            <Icon name={icon} size={20} color="#666360" />
+        <Container isFocused={isFocused}>
+            <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
             <TextInput
                 ref={inputElementRef}
                 keyboardAppearance="dark"
                 placeholderTextColor="#666360"
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
                 defaultValue={defaultValue}
                 onChangeText={(value) => {
                     inputValueRef.current.value = value;
