3a7ff628ec8633e43c25d7c829c9588e00dfc352
Simão Menezes <simaomenezesneto@gmail.com>
2020-05-10 15:46:57 +0100

    Testando criação de usuario

---
+++ nivel_04/back-end-gobarber/src/modules/users/repositories/Fakes/FakeUserRepository.ts
@@ -0,0 +1,40 @@
+
+import User from "../../infra/typeorm/entities/User";
+import { uuid } from "uuidv4";
+import ICreateUserDTO from "@modules/users/dtos/ICreateUserDTO";
+import IUserRepository from "../IUserRepository";
+
+class UsersRepository implements IUserRepository {
+
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.id === id);
+    return findUser;
+  }
+
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.email === email);
+    return findUser;
+  }
+
+
+  public async create(dataUser: ICreateUserDTO): Promise<User> {
+    const user = new User();
+    Object.assign(user, { id: uuid() }, dataUser);
+    this.users.push(user);
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findUser = this.users.findIndex(u => u.id === user.id);
+
+    this.users[findUser] = user;
+
+    return user;
+  }
+
+}
+
+export default UsersRepository;
---
+++ nivel_04/back-end-gobarber/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,44 @@
+import FakeUserRepository from '../repositories/Fakes/FakeUserRepository';
+import CreateUserService from './CreateUserService';
+
+import AppError from '@shared/errors/AppError';
+
+
+describe('CreateUser', () => {
+
+  it('should be able to create a new user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const createUserService = new CreateUserService(fakeUserRepository);
+
+    const user = await createUserService.execute({
+      name: 'Zezinho Lima',
+      email: 'zezinho@gmail.com',
+      password: '121212'
+    });
+
+    expect(user).toHaveProperty('id');
+    expect(user.email).toBe('zezinho@gmail.com');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const createUserService = new CreateUserService(fakeUserRepository);
+
+    const user = await createUserService.execute({
+      name: 'Zezinho Lima',
+      email: 'zezinho@gmail.com',
+      password: '121212'
+    });
+
+    expect(
+      createUserService.execute({
+        name: 'Zezinho Lima',
+        email: 'zezinho@gmail.com',
+        password: '121212'
+      }),
+
+    ).toBeInstanceOf(AppError);
+  });
+
+});
+
