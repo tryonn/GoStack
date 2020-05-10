4c55e424b6cec7d38a5a3265ffadb4982b4a2599
Simão Menezes <simaomenezesneto@gmail.com>
2020-05-10 14:45:44 +0100

    Criando o primeiro teste

---
+++ nivel_04/back-end-gobarber/src/modules/appointments/repositories/fakes/FakesAppoimentsRepository.ts
@@ -0,0 +1,36 @@
+import { uuid } from 'uuidv4';
+import Appointment from "../../infra/typeorm/entities/Appointment";
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentsDTO from '@modules/appointments/dtos/ICreateAppointmentsDTO';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+
+  private appointments: Appointment[] = [];
+
+  public async create({ provider_id, date }: ICreateAppointmentsDTO): Promise<Appointment> {
+
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id })
+
+    /*
+    appointment.id = uuid();
+    appointment.provider_id = provider_id;
+    appointment.date = date;
+    */
+    this.appointments.push(appointment);
+
+
+    return appointment;
+  }
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+    return findAppointment;
+  }
+
+}
+
+export default AppointmentsRepository;
--- nivel_04/back-end-gobarber/src/modules/appointments/services/CreateAppoimentService.spec.ts
+++ nivel_04/back-end-gobarber/src/modules/appointments/services/CreateAppoimentService.spec.ts
@@ -1,3 +1,25 @@
-test('sum two number', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakesAppoimentsRepository';
+import CreateAppointmentServices from './CreateAppointmentService';
+
+
+describe('CreateAppointment', () => {
+
+  it('should be able to create a new appoinment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentServices = new CreateAppointmentServices(fakeAppointmentsRepository);
+
+    const appointment = await createAppointmentServices.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
+
+
+
+
+  });
+
 });
+
