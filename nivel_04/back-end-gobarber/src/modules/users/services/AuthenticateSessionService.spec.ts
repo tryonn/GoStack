ce714098bc3e6a1a70bf7da13cba5894e908f0d9
Simão Menezes <simaomenezesneto@gmail.com>
2020-05-10 18:50:29 +0100

    Testes de autenticação

--- nivel_04/back-end-gobarber/src/modules/users/services/AuthenticateSessionService.spec.ts
+++ nivel_04/back-end-gobarber/src/modules/users/services/AuthenticateSessionService.spec.ts
@@ -9,6 +9,7 @@ import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 describe('AuthenticateSessionService', () => {
 
   it('should be able to authenticate', async () => {
+
     const fakeUserRepository = new FakeUserRepository();
     const fakeHashProvider = new FakeHashProvider();
 
@@ -29,6 +30,52 @@ describe('AuthenticateSessionService', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authService = new AuthenticateSessionService(fakeUserRepository, fakeHashProvider);
+    const createUserService = new CreateUserService(fakeUserRepository, fakeHashProvider);
+
+    await createUserService.execute({
+      name: 'Zezinho Lima',
+      email: 'zezinho@gmail.com',
+      password: '121212'
+    });
+
+    expect(authService.execute({
+      email: 'zezin1ho@gmail.com',
+      password: '1211212'
+    }),
+    ).rejects.toBeInstanceOf(AppError);
+
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authService = new AuthenticateSessionService(fakeUserRepository, fakeHashProvider);
+    const createUserService = new CreateUserService(fakeUserRepository, fakeHashProvider);
+
+
+    await createUserService.execute({
+      name: 'Zezinho Lima',
+      email: 'zezinho@gmail.com',
+      password: '121212'
+    });
+
+    expect(
+      authService.execute({
+        email: 'zezinho@gmail.com',
+        password: '222222'
+      }),
+    ).rejects.toBeInstanceOf(AppError);
 
   });
 
