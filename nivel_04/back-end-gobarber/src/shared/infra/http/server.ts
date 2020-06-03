c95221db203473636500da5da3ceead4fe793759
Simão Menezes <simaomenezesneto@gmail.com>
2020-06-03 22:10:18 +0100

    Express rate limit

--- nivel_04/back-end-gobarber/src/shared/infra/http/server.ts
+++ nivel_04/back-end-gobarber/src/shared/infra/http/server.ts
@@ -6,6 +6,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
+import rateLimiter from './middlewares/rateLimiter';
 import Route from './routes';
 
 import uploadConfig from '@config/upload';
@@ -18,6 +19,7 @@ import { errors } from 'celebrate';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
