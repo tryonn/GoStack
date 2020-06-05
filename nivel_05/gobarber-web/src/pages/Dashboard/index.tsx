a3ce8f1362a5226603128324ba196c1b9dfe8c26
Simão Menezes <simaomenezesneto@gmail.com>
2020-06-05 22:10:09 +0100

    Calendário e estilizações

--- nivel_05/gobarber-web/src/pages/Dashboard/index.tsx
+++ nivel_05/gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppontment, Section, Appointment, Calendar } from './styles';
 
@@ -9,6 +11,12 @@ import { useAuth } from '../../hook/Auth';
 const Dashboard: React.FC = () => {
 
     const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+        if (modifiers.available) {
+            setSelectedDate(day);
+        }
+    }, []);
 
     // <img src={user.avatar_url} alt={user.name} />
 
@@ -110,7 +118,30 @@ const Dashboard: React.FC = () => {
 
                 </Schedule>
                 <Calendar>
-
+                    <DayPicker
+                        weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S',]}
+                        fromMonth={new Date()}
+                        disabledDays={[{ daysOfWeek: [0, 6] }]}
+                        modifiers={{
+                            available: { daysOfWeek: [1, 2, 3, 4, 5] },
+                        }}
+                        selectedDays={selectedDate}
+                        onDayClick={handleDateChange}
+                        months={[
+                            'Janeiro',
+                            'Fevereiro',
+                            'Março',
+                            'Abril',
+                            'Naio',
+                            'Junho',
+                            'Julho',
+                            'Agosto',
+                            'Setembro',
+                            'Outubro',
+                            'Novembro',
+                            'Dezembro',
+                        ]}
+                    />
                 </Calendar>
             </Content>
         </Container>
